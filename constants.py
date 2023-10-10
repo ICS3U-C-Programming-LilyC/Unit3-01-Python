6ad602700589158815083989c641e1ce50b618b3
Lily Carroll <lily.carroll@stu.ocsb.ca>
2023-10-10 17:09:22 +0000

    finished

---
+++ constants.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# Created by: Lily Carroll
+# Created on: Oct/10/2023
+# This program contain the HST constant.
+
+HST = 0.11
---
+++ tax.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# Created by: Lily Carroll
+# Created on: Oct/10/2023
+# This program calculates the total using the HST tax in Saskatchewan.
+
+import constants
+
+def main():
+  # Get user input for the subtotal.
+  subtotal = int(input("Enter your subtotal = $ ")) 
+
+  # Calculate the tax.
+  tax = subtotal * constants.HST
+
+  # Calculate the total.
+  total = subtotal + tax
+
+  # Display the tax back to the user.
+  print("")
+  print("The tax would ben = ${:,.2f}".format(tax))
+
+  # Display the total back to the user.
+  print("")
+  print("The total cost is = ${:,.2f}".format(total))
+
+if __name__ == "__main__":
+    main()
+  
